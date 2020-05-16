bb6df73052011e720acdffbf9787c135ea824350
Rupesh Chavan <rupesh.chavan007@gmail.com>
2020-05-16 18:04:08 +0530

    first commit

---
+++ src/main/java/com/chavan/rupeshspringboot/RupeshSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.chavan.rupeshspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupeshSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RupeshSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chavan/rupeshspringboot/RupeshSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chavan.rupeshspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupeshSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
